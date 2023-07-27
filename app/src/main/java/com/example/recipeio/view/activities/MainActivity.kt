a4a98e3772a728a247814dd26dd3eb0fabb932b5
Chermen12345 <bekuzarovchermen@gmail.com>
2023-07-27 12:03:45 +0400

    set Up dependecies architecture and login navigation

---
+++ app/src/main/java/com/example/recipeio/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recipeio.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recipeio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
